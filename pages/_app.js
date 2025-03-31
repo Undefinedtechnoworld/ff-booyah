6b0b831c2e9e288c87b20eb371af495350c588b7
Undefinedtechnoworld <undefinedtechnoworld@gmail.com>
2025-03-31 18:16:37 +0530

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
 import { GA4Initializer } from "@components/gtag";
 import "../styles/globals.css";
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    import("react-onesignal").then((OneSignal) => {
+      OneSignal.default.init({
+        appId: "5af166ef-dcda-406e-adce-63a790a18655",
+        serviceWorkerPath: "/OneSignalSDKWorker.js",
+      });
+    });
+  }, []);
+
   return (
     <Fragment>
       <GA4Initializer />
--- pages/_document.js
+++ pages/_document.js
@@ -12,36 +12,33 @@ export default function Document() {
           type="text/css"
           href="https://cdn.jsdelivr.net/npm/neuicons/css/neu.min.css"
         />
-        
-        {/* OneSignal Push Notification Script */}
         <script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.OneSignalDeferred = window.OneSignalDeferred || [];
-              OneSignalDeferred.push(async function(OneSignal) {
-                await OneSignal.init({
-                  appId: "5af166ef-dcda-406e-adce-63a790a18655",
-                });
-              });
-            `,
-          }}
-        />
-
         {/* Google Tag Manager Script */}
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N4HX3QFB');
+          `,
+        }} />
         <script
+          id="onesignal-init"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-N4HX3QFB');
-            `,
+          window.OneSignalDeferred = window.OneSignalDeferred || [];
+          OneSignalDeferred.push(async function(OneSignal) {
+            await OneSignal.init({
+              appId: "5af166ef-dcda-406e-adce-63a790a18655",
+            });
+          });
+        `,
           }}
         />
       </Head>
-      
+
       <body>
         {/* Google Tag Manager (noscript) */}
         <noscript>
