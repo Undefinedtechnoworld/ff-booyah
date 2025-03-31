7c6ba7d023fce8a3716595630fbf13a2a4dd71b6
Undefinedtechnoworld <undefinedtechnoworld@gmail.com>
2025-03-31 16:37:38 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import { GA4Initializer } from "@components/gtag";
 import "../styles/globals.css";
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import OneSignal from "react-onesignal";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    OneSignal.init({
+      appId: "5af166ef-dcda-406e-adce-63a790a18655",
+      serviceWorkerPath: "/OneSignalSDKWorker.js",
+    });
+  }, []);
+
   return (
     <Fragment>
       <GA4Initializer />
